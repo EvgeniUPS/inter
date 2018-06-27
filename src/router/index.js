7ff8e5ab6ecb63f9a6302d25272dce69ab11983d
EvgeniUPS <evgeniups@gmail.com>
2018-06-28 01:29:43 +0300

    add temp component

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Temp from '@/components/Temp'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/temp',
+      name: 'Temp',
+      component: Temp
+    },
+    {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
-  ]
+    },
+  ],
+  mode: 'history'
 })
