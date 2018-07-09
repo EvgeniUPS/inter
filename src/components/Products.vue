15211d46623bb741920ef4f9a2e29564aa6c4c3b
EvgeniUPS <evgeniups@gmail.com>
2018-07-09 20:40:27 +0300

    add json-server

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -12,24 +12,66 @@
 
     <div class="form-group">
       <label for="id">Product Id</label>
-      <input type="text" id="id" class="form-control" v-model.number="productId">
+      <input type="text" id="productId" class="form-control" v-model.number="productId">
     </div>
 
     <button class="btn btn-success" @click="createProduct">Создать продукт</button>
+    <button class="btn btn-primary" @click="loadProducts">Показать продукты</button>
+
+    <hr>  
+    <ul class="list-group">
+      <li 
+      class="list-group-item"
+      v-for="product of products"
+      :key="product.id"
+      >
+        <strong> {{product.factory}} - {{product.model}} - {{product.productId}} </strong>
+      </li>
+    </ul>
   </div>
 </template>
+
+
+
 <script>
 export default {
   data() {
     return {
       productFactory: "",
       productModel: "",
-      productId: 2018
+      productId: 2018,
+      products: []
     };
   },
   methods: {
     createProduct() {
       console.log("create Product");
+      const product = {
+        factory: this.productFactory,
+        model: this.productModel,
+        productId: this.productId
+      };
+      this.$http
+        .post("http://localhost:3000/products", product)
+        .then(response => {
+          // console.log(response);
+          return response.json();
+        })
+        .then(newProduct => {
+          console.log(newProduct);
+        });
+    },
+    loadProducts() {
+      this.$http
+        .get("http://localhost:3000/products")
+        .then(response => {
+          // console.log(response);
+          return response.json();
+        })
+        .then(products => {
+          this.products = products;
+          // console.log(products);
+        });
     }
   }
 };
